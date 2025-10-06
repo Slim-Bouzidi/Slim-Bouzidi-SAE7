a61c371c7569f631f5fa853adf25a967fe3dc070
Sliim Bouzidi <sliimbouzidi@gmail.com>
2025-10-06 14:22:00 +0100

    Initial Maven project setup

---
+++ src/test/java/com/slimbouzidi/sae7/Sae7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slimbouzidi.sae7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sae7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
